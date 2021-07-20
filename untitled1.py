c6a73dbb322124442f62f50ee0e408781f8bba57
mira0907 <87653245+mira0907@users.noreply.github.com>
2021-07-20 11:03:31 +0800

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 20:20:01 2021
+
+@author: dkjua
+"""
+
+print("請輸入半徑的值：　")
+x = float(input())
+y = x * 3.1415926 * 2.0
+print("圓周長 = " + str(y) )
